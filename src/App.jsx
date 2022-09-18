f093a40fd7cc26036c2ab90aa3a3d04f56b8fd0f
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-18 15:03:48 +0630

    upPhoto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Header } from './components'
+
 
 const App = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center text-blue-600'>
-       app
+    <div className='w-screen h-auto flex-col'>
+        <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='fiexd z-50 w-screen bg-slate-300 p-6 px-16'>
+        {/* desktop and tablet */}
+        <div className='hidden md:flex w-full h-full '>
+            
+        </div>
+        {/* mobile */}
+        <div className='flex md:hidden w-full h-full '>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
