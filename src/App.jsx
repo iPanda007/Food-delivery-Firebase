814482a68b3ec11ad1302bcb5d3ccdc6f1065148
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-18 14:04:46 +0630

    iDepnecery

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='w-screen h-screen flex items-center justify-center text-blue-600'>
+       app
     </div>
   )
 }
