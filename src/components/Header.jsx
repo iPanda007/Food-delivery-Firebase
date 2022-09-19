7bbbb6d08a8200327c200575b03c145e671f3e2a
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-19 23:37:00 +0630

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Logo from '../img/logo.png';
 import {MdShoppingBasket,MdAdd,MdLogout} from 'react-icons/md';
 import {Link} from 'react-router-dom'
@@ -9,6 +9,7 @@ import { app } from '../firebase.config';
 import { useStateValue } from '../context/StateProvider';
 const Header = () => {
     //instance
+    const [isMenu,setMenu] = useState(false);
     const {initialState,dispatchReducer} = useStateValue();
     const {user} = initialState
     
@@ -24,6 +25,8 @@ const Header = () => {
             user:providerData[0]
           })
         localStorage.setItem("user",JSON.stringify(providerData[0]))
+         }else{
+          setMenu(!isMenu)
          }
     }
   return (
@@ -54,16 +57,20 @@ const Header = () => {
                      src={user ? user.photoURL : Avatar} className='min-w-[40px] min-h-[40px] w-10 h-10 cursor-pointer rounded-full ' 
                      onClick={login}
              />
-                <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-4 '>
-                  {
-                    user && user.email === "aungkyawkhaing2000@gmail.com" && (
-                      <Link to={'/createitem'}>
-                                   <p  className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >New Item    <MdAdd/></p>
-                      </Link>
-                    )
-                  }
-                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >Logout    <MdLogout/></p>
-                </div>
+             
+              
+                <div className={`w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-4 transition-all duration-500 ${isMenu ? 'scale-100':'scale-0'}`} >
+                {
+                  user && user.email === "aungkyawkhaing2000@gmail.com" && (
+                    <Link to={'/createitem'}>
+                                 <p  className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >New Item    <MdAdd/></p>
+                    </Link>
+                  )
+                }
+                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >Logout    <MdLogout/></p>
+              </div>
+
+               
                </div>
               
             </div>
