2158cb0a033b58da36a58be6790fc5078c38c64d
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-20 00:57:59 +0630

    Logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,6 +29,14 @@ const Header = () => {
           setMenu(!isMenu)
          }
     }
+    const logout = ()=>{
+      setMenu(false)
+      localStorage.clear()
+      dispatchReducer({
+        type:"SET_USER",
+        user:null
+      })
+    }
   return (
     <header className='fiexd z-50 w-screen  p-3 px-4 md:p-6 md:px-16'>
         {/* desktop and tablet */}
@@ -67,7 +75,9 @@ const Header = () => {
                     </Link>
                   )
                 }
-                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >Logout    <MdLogout/></p>
+                  <p
+                    onClick={logout} 
+                    className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >Logout    <MdLogout/></p>
               </div>
 
                
@@ -89,7 +99,7 @@ const Header = () => {
              />
              
               
-                <div className={`w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-4 transition-all duration-500 ${isMenu ? 'scale-100':'scale-0'}`} >
+                <div className={` overflow-hidden  w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-4 transition-all duration-500 ${isMenu ? 'scale-100':'scale-0'}`} >
                 {
                   user && user.email === "aungkyawkhaing2000@gmail.com" && (
                     <Link to={'/createitem'}>
@@ -106,7 +116,11 @@ const Header = () => {
                         <li className=' px-4 py-2 text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100 transition-all ease-in-out cursor-pointer '>About Us</li>
                         <li className=' px-4 py-2 text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100 transition-all ease-in-out cursor-pointer '>Service</li>
                     </ul>
-                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >Logout    <MdLogout/></p>
+                  <p
+                  onClick={logout} 
+                  className='px-6 py-2 m-2 rounded-md shadow-sm flex items-center justify-center gap-3 cursor-pointer  bg-slate-200 hover:bg-slate-300 transition-all duration-200 ease-in-out text-textColor text-base' >
+                    Logout <MdLogout/>
+                  </p>
               </div>
             </div>
         </div>
--- src/utils/FetchLocalStroageData.jsx
+++ src/utils/FetchLocalStroageData.jsx
@@ -1,5 +1,5 @@
 export const fetchUser = ()=>{
-     const userInfo = localStorage.getItem('user') !== "undefined"
+     const userInfo = localStorage.getItem('user') !== "null"
                         ? JSON.parse(localStorage.getItem('user'))
                          : localStorage.clear()
 
