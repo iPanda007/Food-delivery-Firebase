49033a62852267d5be6c24a06e7ea3afbf277eb9
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-20 00:12:44 +0630

    Mobile Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
          }
     }
   return (
-    <header className='fiexd z-50 w-screen  p-6 px-16'>
+    <header className='fiexd z-50 w-screen  p-3 px-4 md:p-6 md:px-16'>
         {/* desktop and tablet */}
         <div className='hidden md:flex w-full h-full 
         items-center justify-between'>
@@ -76,8 +76,39 @@ const Header = () => {
             </div>
         </div>
         {/* mobile */}
-        <div className='flex md:hidden w-full h-full drop-shadow-lg '>
-
+        <div className='flex md:hidden w-full h-full drop-shadow-lg items-center justify-between  '>
+          <Link to={'/'} className='flex items-center gap-2'>
+                  <img src={Logo} className="w-8 object-cover" alt="logo"/>
+                  <p className='text-headingColor text-xl font-bold'>City</p>
+            </Link>
+            <div className='relative' > 
+               <motion.img
+                     whileTap={{scale:0.6}}
+                     src={user ? user.photoURL : Avatar} className='min-w-[40px] min-h-[40px] w-10 h-10 cursor-pointer rounded-full ' 
+                     onClick={login}
+             />
+             
+              
+                <div className={`w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-4 transition-all duration-500 ${isMenu ? 'scale-100':'scale-0'}`} >
+                {
+                  user && user.email === "aungkyawkhaing2000@gmail.com" && (
+                    <Link to={'/createitem'}>
+                                 <p  className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >
+                                  New Item    <MdAdd/>
+                                  
+                                  </p>
+                    </Link>
+                  )
+                }
+                  <ul className='flex-col  '>
+                        <li className=' px-4 py-2 text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100 transition-all ease-in-out cursor-pointer '>Home</li>
+                        <li className=' px-4 py-2 text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100 transition-all ease-in-out cursor-pointer '>Menu</li>
+                        <li className=' px-4 py-2 text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100 transition-all ease-in-out cursor-pointer '>About Us</li>
+                        <li className=' px-4 py-2 text-base text-textColor hover:text-headingColor hover:bg-slate-100 duration-100 transition-all ease-in-out cursor-pointer '>Service</li>
+                    </ul>
+                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base' >Logout    <MdLogout/></p>
+              </div>
+            </div>
         </div>
     </header>
   )
