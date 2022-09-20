bc8d72deccfad4b8fca081c50ba5e5009ca1e48f
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-20 20:55:19 +0630

    changePlace

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -87,17 +87,20 @@ const Header = () => {
         </div>
         {/* mobile */}
         <div className='flex md:hidden w-full h-full drop-shadow-lg items-center justify-between  '>
-          <Link to={'/'} className='flex items-center gap-2'>
-                  <img src={Logo} className="w-8 object-cover" alt="logo"/>
-                  <p className='text-headingColor text-xl font-bold'>City</p>
-            </Link>
-                  
-            <div className= ' relative flex items-center justify-center'>
+
+        <div className= ' relative flex items-center justify-center'>
                  <MdShoppingBasket className='text-textColor text-2xl '/>
                  <div className=' absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
                     <p className='text-xs text-white font-semibold'>2</p>
                  </div>
             </div>
+
+          <Link to={'/'} className='flex items-center gap-2'>
+                  <img src={Logo} className="w-8 object-cover" alt="logo"/>
+                  <p className='text-headingColor text-xl font-bold'>City</p>
+            </Link>
+                  
+         
             <div className='relative' > 
                <motion.img
                      whileTap={{scale:0.6}}
