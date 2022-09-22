5caf91f63d75092996cd285d688feed8eac74518
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-22 22:28:42 +0630

    DataUpdate

---
+++ src/utils/Data.jsx
@@ -0,0 +1,13 @@
+
+import I1 from '../img/i1.png';
+import F1 from '../img/f1.png';
+import C3 from '../img/c3.png'
+
+const heropData =[
+
+
+    {id:1,name:'Icecream',decp:'chocolate & vaniall',price:'5.25',imageSrc:I1},
+    {id:2,name:'Strawberries',decp:'Fresh Strawberries',price:'10.25',imageSrc:F1},
+    {id:3,name:'Chicken kebab',decp:'Mixed Kebab Plate',price:'5.25',imageSrc:C3},
+    {id:4,name:'Icecream',decp:'chocolate & vaniall',price:'5.25',imageSrc:''},
+]
